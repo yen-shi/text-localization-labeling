a26fc1bbfa6b695dab6ef6fdeef9dd831c9e9a32
yen-shi <dannywang0802@yahoo.com.tw>
2018-04-18 00:46:39 +0800

    Complete version 1.0

--- server.js
+++ server.js
@@ -1,12 +1,121 @@
-var express = require('express');
-var app = express();
-var http = require('http').Server(app);
+const fs = require('fs');
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const bodyParser = require('body-parser')
+const readline = require('readline');
+
+let number = 1, tot = 3;
+
+// parse application/json
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use('/src', express.static(__dirname + '/src'));
 app.use('/cases', express.static(__dirname + '/cases'));
 
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/api/number', function(req, res) {
+  // This function is unsafe !
+  res.json(number);
+  number = (number % tot) + 1;
+});
+
+app.get('/api/imglists/:number', function(req, res) {
+  // This function is unsafe !
+  let target_dir = 'cases/' + req.params.number.toString() + '/';
+  console.log('Get: ', req.params);
+  fs.readdir(target_dir, (err, files) => {
+    for(let i = 0; i < files.length; i++)
+      files[i] = target_dir + files[i];
+    // console.log(files);
+    res.json(files);
+  });
+});
+
+// Read line by line
+// https://stackoverflow.com/questions/6156501/read-a-file-one-line-at-a-time-in-node-js
+
+function readLabel(name) {
+  return new Promise((resolve, reject) => {
+    let ss = name.split('/');
+    let dest = 'labels/' + ss[1] + '/' + ss[2].split('.')[0] + '.txt';
+    let boxes = [];
+    if (fs.existsSync(dest)) {
+      fs.readFile(dest, 'utf8', function(err, data) {
+        let lines = data.split('\n');
+        lines.forEach((line) => {
+          let box = [];
+          let ls = line.split(',');
+          if (ls.length == 9) {
+            for(let i = 0; i < 8; i++)
+              box.push(parseInt(ls[i]));
+            boxes.push(box);
+          }
+        });
+        // console.log('Read boxes: ', boxes);
+        console.log('Boxes read from: ', dest);
+        resolve(boxes);
+      });
+      // rd = readline.createInterface({
+        // input: fs.createReadStream(dest)
+      // });
+      // rd.on('line', function(line) {
+        // // console.log('Get line: ', line);
+        // box = [];
+        // ls = line.split(',');
+        // for(let i = 0; i < 8; i++)
+          // box.push(parseInt(ls[i]));
+        // boxes.push(box);
+      // });
+      // rd.on('close', function() {
+        // // console.log('Read boxes: ', boxes);
+        // resolve(boxes);
+      // });
+    }
+    else {
+      resolve(boxes);
+    }
+  });
+}
+
+function writeLabel(name, boxes) {
+  return new Promise((resolve, reject) => {
+    let ss = name.split('/');
+    let dest = 'labels/' + ss[1] + '/' + ss[2].split('.')[0] + '.txt';
+    let output = '';
+    // console.log('Write boxes: ', boxes);
+    for(let i = 0; i < boxes.length; i++) {
+      for(let j = 0; j < boxes[i].length; j++) {
+        output += boxes[i][j].toString() + ',';
+      }
+      output += '###\n';
+    }
+    fs.writeFile(dest, output, (err) => {
+      if (err) throw err;
+      resolve(dest);
+    });
+  });
+}
+
+app.post('/api/imglabel', function(req, res) {
+  // This function is unsafe !
+  // console.log('Post: ', req.params);
+  // console.log('Dest: ', dest);
+  // console.log('Name: ', name);
+  // console.log('Boxes: ', boxes);
+  const { name, boxes, next } = req.body;
+  console.log('Get name: ', name);
+  console.log('Get next: ', next);
+  if (name != 'init') {
+    writeLabel(name, boxes)
+      .then((dest) => console.log('Result is written to ', dest));
+  }
+  readLabel(next)
+    .then((ret) => res.json(ret));
 });
 
 http.listen(3000, function(){
--- src/index.js
+++ src/index.js
@@ -1,37 +1,128 @@
 // Ref: https://stackoverflow.com/questions/30565987/cropping-images-with-html5-canvas-in-a-non-rectangular-shape-and-transforming
+let number = 1;
 let canvas = document.getElementById('main-canvas');
-let context = canvas.getContext('2d');
 let showName = document.getElementById('filename');
+let process = document.getElementById('process');
+let slider = document.getElementById("height-slider");
+let showValue = document.getElementById("slider-val");
+let context = canvas.getContext('2d');
+let boxHeight = 30;
 
-imgList = ['cases/case-1.png', 'cases/case-2.png', 'cases/case-3.png', 'cases/case-4.png', 'cases/case-5.png']
-nowIdx = 0;
-
-// load example image
-let img = new Image();
-img.onload = start;
-img.src = imgList[nowIdx];
-showName.innerHTML = imgList[nowIdx];
-
-function start() {
-
-  // set canvas sizes equal to image size
-  cw=canvas.width=canvas.width=img.width;
-  ch=canvas.height=canvas.height=img.height;
-
-  // draw the example image on the source canvas
-  context.drawImage(img,0,0);
-
-}
-
-// Ref: http://www.williammalone.com/articles/create-html5-canvas-javascript-drawing-app/
 let boxes = []
 let box = []
 let change = false;
 let nowX = -1, nowY = -1;
+let isDrawing = false;
+
+let imgList = [];
+let nowIdx = 0;
+let img = new Image();
+img.onload = start;
+
+showValue.innerHTML = slider.value;
+
+slider.oninput = function() {
+    showValue.innerHTML = this.value;
+    boxHeight = this.value;
+    redraw();
+}
+
+window.onkeyup = function(e) {
+  let key = e.keyCode ? e.keyCode : e.which;
+  // console.log('Get key : ', key);
+
+  if (key == 37) {
+      boxHeight -= 1;
+  } else if (key == 39) {
+      boxHeight += 1;
+  }
+  showValue.innerHTML = boxHeight;
+  start();
+}
+
+/////////////////////////////////// Get and Post methods /////////////////////////////////
+function getList(url) {
+  return fetch(url, {
+    cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+    credentials: 'same-origin', // include, same-origin, *omit
+    headers: {
+      'user-agent': 'Mozilla/4.0 MDN Example',
+      'content-type': 'application/json'
+    },
+    method: 'GET', // *GET, POST, PUT, DELETE, etc.
+    mode: 'cors', // no-cors, cors, *same-origin
+    redirect: 'follow', // manual, *follow, error
+    referrer: 'no-referrer', // *client, no-referrer
+  })
+  .then(response => response.json()); // parses response to JSON
+}
+
+// $.ajax({
+//   type: 'POST',
+//   url: '/api/imglabel',
+//   data: { 'name': imgList[nowIdx], 'boxes': boxes },
+//   dataType: 'application/json',
+//   success: function() { console.log('Post success'); }
+// });
+
+function postLabel(url, data) {
+  // console.log('Post data: ', data);
+  return fetch(url, {
+    body: JSON.stringify(data), // must match 'Content-Type' header
+    cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+    credentials: 'same-origin', // include, same-origin, *omit
+    headers: {
+      'user-agent': 'Mozilla/4.0 MDN Example',
+      'content-type': 'application/json'
+    },
+    method: 'POST', // *GET, POST, PUT, DELETE, etc.
+    mode: 'cors', // no-cors, cors, *same-origin
+    redirect: 'follow', // manual, *follow, error
+    referrer: 'no-referrer', // *client, no-referrer
+  });
+}
+
+fetch('/api/number', {
+  cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+  credentials: 'same-origin', // include, same-origin, *omit
+  headers: {
+    'user-agent': 'Mozilla/4.0 MDN Example',
+    'content-type': 'application/json'
+  },
+  method: 'GET', // *GET, POST, PUT, DELETE, etc.
+  mode: 'cors', // no-cors, cors, *same-origin
+  redirect: 'follow', // manual, *follow, error
+  referrer: 'no-referrer', // *client, no-referrer
+})
+.then(response => response.json()) // parses response to JSON
+.then(data => { console.log('Get number: ', data); number = data; })
+.then(() => {
+  getList('/api/imglists/' + number.toString())
+  .then(data => { console.log('Get list: ', data); imgList = data; })
+  .then(() => {
+    postLabel('/api/imglabel', { 'name': 'init',
+                                 'boxes': [],
+                                 'next': imgList[nowIdx] })
+      .then((res) => res.json())
+      .then((res) => {
+        process.innerHTML = '(' + imgList.length.toString() + '/' + (nowIdx+1).toString() + '):';
+        console.log('Init boxes: ', res);
+        box = [];
+        boxes = res;
+        img.src=imgList[nowIdx];
+        showName.innerHTML = imgList[nowIdx];
+        redraw();
+        console.log('Complete initialization.');
+      });
+  });
+});
+
+/////////////////////////////////// Canvas operations //////////////////////////////////////
+// Ref: http://www.williammalone.com/articles/create-html5-canvas-javascript-drawing-app/
 
 $('#main-canvas').mousedown(function(e){
-  var mouseX = e.pageX - this.offsetLeft;
-  var mouseY = e.pageY - this.offsetTop;
+  let mouseX = e.pageX - this.offsetLeft;
+  let mouseY = e.pageY - this.offsetTop;
 
   addClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop);
 });
@@ -39,7 +130,7 @@ $('#main-canvas').mousedown(function(e){
 $('#main-canvas').mousemove(function(e){
   nowX = e.pageX - this.offsetLeft;
   nowY = e.pageY - this.offsetTop;
-  redraw();
+  start();
 });
 
 $('#main-canvas').mouseup(function(e){
@@ -61,41 +152,117 @@ $('#undo-button').click(function () {
     box = [];
   else if (boxes.length != 0)
     boxes.pop();
-  start();
   redraw();
 });
 
-$('#next-button').click(function () {
-  box = [];
-  boxes = [];
-  nowIdx++;
-  nowIdx %= imgList.length;
-  img.src=imgList[nowIdx];
-  showName.innerHTML = imgList[nowIdx];
-});
-
-function addClick(x, y)
-{
-  console.log('Add (x, y) = (', x, ', ', y, ')');
-  box.push(x);
-  box.push(y);
-  if (box.length == 8) {
-    boxes.push(box);
-    box = [];
-  }
+function changeImg(nextIdx) {
+  process.innerHTML = '(' + imgList.length.toString() + '/' + (nextIdx+1).toString() + '):';
+  completeBox(boxHeight)
+    .then(() => {
+      console.log('post: ', boxes);
+      postLabel('/api/imglabel', { 'name': imgList[nowIdx], 
+                                   'boxes': boxes,
+                                   'next': imgList[nextIdx] })
+      .then((res) => res.json())
+      .then((res) => {
+        console.log('Post return: ', res);
+        // boxes = [];
+        box = [];
+        boxes = res;
+        nowIdx = nextIdx;
+        img.src = imgList[nowIdx];
+        showName.innerHTML = imgList[nowIdx];
+        redraw();
+      });
+    });
 }
 
-function redraw(){
+$('#next-button').click(function () {
+  changeImg((nowIdx+1) % imgList.length);
+});
+
+$('#prev-button').click(function () {
+  changeImg((nowIdx-1+imgList.length) % imgList.length);
+});
+
+/////////////////////////////////// Canvas functions //////////////////////////////////////
+function addClick(x, y)
+{
+  // console.log('Add (x, y) = (', x, ', ', y, ')');
+  if (box.length == 4) {
+    completeBox(boxHeight);
+  }
+  box.push(x);
+  box.push(y);
+  if (box.length == 2)
+    isDrawing = true;
+  else
+    isDrawing = false;
+}
+
+function completeBox(len) {
+  return new Promise((resolve, reject) => {
+    if (box.length == 4) {
+      let aX = box[1] - box[3];
+      let aY = box[2] - box[0];
+      let tot = Math.pow( Math.pow(aX, 2) + Math.pow(aY, 2), 0.5);
+      aX = Math.round(aX * len / tot);
+      aY = Math.round(aY * len / tot);
+      box.push(box[2] + aX);
+      box.push(box[3] + aY);
+      box.push(box[0] + aX);
+      box.push(box[1] + aY);
+      boxes.push(box);
+      box = [];
+    }
+    resolve();
+  });
+}
+
+function getBox(len) {
+  let aX = box[1] - box[3];
+  let aY = box[2] - box[0];
+  let tot = Math.pow( Math.pow(aX, 2) + Math.pow(aY, 2), 0.5);
+  let newBox = box.slice();
+  aX = Math.round(aX * len / tot);
+  aY = Math.round(aY * len / tot);
+  newBox.push(box[2] + aX);
+  newBox.push(box[3] + aY);
+  newBox.push(box[0] + aX);
+  newBox.push(box[1] + aY);
+  return newBox;
+}
+
+function start() {
+  // set canvas sizes equal to image size
+  canvas.width=img.width;
+  canvas.height=img.height;
+
+  // draw the example image on the source canvas
+  context.drawImage(img,0,0);
+  redraw();
+}
+
+function redraw() {
   // context.clearRect(0, 0, context.canvas.width, context.canvas.height); // Clears the canvas
   context.strokeStyle = "#aa7777";
   context.lineJoin = "round";
   context.lineWidth = 2;
 
-  if (box.length != 0) {
+  if (box.length == 2) {
     context.beginPath();
     context.moveTo(box[0], box[1]);
-    for(let i = 2; i < box.length; i = i+2)
-      context.lineTo(box[i], box[i+1]);
+    if (nowX != -1)
+      context.lineTo(nowX, nowY);
+    context.stroke();
+  }
+  else if (box.length == 4) {
+    let tmp = getBox(boxHeight);
+    context.beginPath();
+    context.moveTo(tmp[0], tmp[1]);
+    for(let j = 2; j < 8; j = j+2)
+      context.lineTo(tmp[j], tmp[j+1]);
+    context.closePath();
     context.stroke();
   }
 
@@ -112,4 +279,3 @@ function redraw(){
   }
 }
 
-// let intervalID = window.setInterval(redraw, 500);
